5af4e54a8c49f953e5fd3f00a96e5fbe93af7c18
rahuleceer <70270506+rahuleceer@users.noreply.github.com>
2022-04-16 19:25:10 +0530

    Update weather.js

--- weather.js
+++ weather.js
@@ -1,6 +1,6 @@
 
 export  async function generateWeather(city){
-        const API_KEY= 'c832136cc3dca2cbc435e2d812f7a5c2' ;
+        const API_KEY=;// put your API_KEY of your openweather account;
         const res= await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`);
         const data= await res.json();
         return data;
